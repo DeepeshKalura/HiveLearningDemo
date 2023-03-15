5bfb0acce074226297e4bdd9411747568c3e4637
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-03-15 17:49:16 +0530

    Make The Important functions
    The Previous commit task Now completed
    I have just implemented Crud operation in the app
    
    Now, I have to make the next screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -32,17 +32,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Box? student;
-
   @override
   void initState() {
     super.initState();
-    openingBox();
   }
 
-  openingBox() async {
-    student = await Hive.openBox('student');
+  Future<String> storingBox(int studentId, Map<String, String> about) async {
+    String response = 'UnSuccessful';
+    try {
+      var student = await Hive.openBox<Map<String, String>>('student');
+
+      student.put(studentId, about);
+
+      response = 'Successful';
+    } catch (e) {
+      response = e.toString();
+    }
+    return response;
   }
+
+  Future<String> readingBox(int studentId) async {
+    String response = 'UnSuccessful';
+    try {
+      var student = await Hive.openBox<Map<String, String>>('student');
+
+      response = student.get(studentId).toString();
+    } catch (e) {
+      response = e.toString();
+    }
+    return response;
+  }
+
+  Future<String> updatingBox(int studentId, Map<String, String> about) async {
+    String response = 'UnSuccessful';
+    try {
+      var student = await Hive.openBox<Map<String, String>>('student');
+
+      student.put(studentId, about);
+
+      response = 'Successful';
+    } catch (e) {
+      response = e.toString();
+    }
+    return response;
+  }
+
+  Future<String> deletingBox(int studentId) async {
+    String response = 'UnSuccessful';
+    try {
+      var student = await Hive.openBox<Map<String, String>>('student');
+
+      student.delete(studentId);
+
+      response = 'Successful';
+    } catch (e) {
+      response = e.toString();
+    }
+    return response;
+  }
+
+  // CRUD Operations functi
 
   @override
   Widget build(BuildContext context) {
@@ -52,18 +101,33 @@ class _MyHomePageState extends State<MyHomePage> {
           title: const Text('Hive Demo 1'),
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
               children: [
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                    ),
+                    onPressed: () {
+                      storingBox(1, {
+                        'name': 'John',
+                        'age': '20',
+                        'address': 'USA',
+                      });
+                    },
                     child: const Text('Create'),
                   ),
                 ),
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                    ),
+                    onPressed: () {
+                      readingBox(1);
+                    },
                     child: const Text('Read'),
                   ),
                 ),
@@ -73,20 +137,90 @@ class _MyHomePageState extends State<MyHomePage> {
               children: [
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                      elevation: MaterialStateProperty.all(3),
+                    ),
+                    onPressed: () {
+                      updatingBox(1, {
+                        'name': 'John',
+                        'age': '20',
+                        'address': 'USA',
+                      });
+                    },
                     child: const Text('Update'),
                   ),
                 ),
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                    ),
+                    onPressed: () {
+                      deletingBox(1);
+                    },
                     child: const Text('Delete'),
                   ),
                 ),
               ],
-            )
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                    ),
+                    onPressed: () {},
+                    child: const Text("View >"),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ViewScreen extends StatelessWidget {
+  const ViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('View Screen'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green),
+                  ),
+                  onPressed: () {},
+                  child: const Text("View >"),
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
